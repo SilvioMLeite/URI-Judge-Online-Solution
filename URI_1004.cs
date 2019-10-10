3b97b4a8fa1b2e81604b592291c2a3d537b874c1
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 20:12:59 -0300

    URI 1004 Solution in C#

---
+++ URI_1004.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1004_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int PROD = valor1 * valor2;
+
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}

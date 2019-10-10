1bd62485eb3a8523eef4a710fe90c4e583d83cdf
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 19:53:43 -0300

    URI 1001 Solution in C#

---
+++ URI_1001.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI_1001_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a + b;
+
+            Console.WriteLine("X = " + x);
+
+        }
+    }
+}

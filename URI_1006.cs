ef5b4cf1529b08e194575f77014b00ccafd885a9
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 20:40:40 -0300

    URI 1006 Solution in C#

---
+++ URI_1006.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1006_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, MEDIA;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = ((A * 2.00) + (B * 3.00) + (C * 5.00)) / ( 2 + 3 + 5);
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1",CultureInfo.InvariantCulture));
+        }
+    }
+}

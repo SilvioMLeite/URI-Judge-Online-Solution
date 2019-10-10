53186588acc2deb786a248a880d03c56ffad1d09
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 20:53:42 -0300

    URI 1007 Solution in C#

---
+++ URI_1007.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI_1007_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[4];
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            int A = vet[0];
+            int B = vet[1];
+            int C = vet[2];
+            int D = vet[3];
+
+            int DIFERENCA = A * B - C * D;
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+        }
+    }
+}

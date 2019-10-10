f144932397ff332e4099bca0d1f287a79ac6c390
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 20:05:59 -0300

    URI 1003 Solution in C#

---
+++ URI_1003.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1003_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int soma = a + b;
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}

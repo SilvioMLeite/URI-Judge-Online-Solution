8dc1c2c3a278d09333fc5200dbabd32ff24737c6
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 19:57:10 -0300

    URI 1002 Solution in C#

---
+++ URI_1002.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1002_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = 3.14159;
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = N * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

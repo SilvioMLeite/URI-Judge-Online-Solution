0c0462ce350ed75223cf14e2594c7ac45b514a1b
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 21:15:35 -0300

    URI 1009 Solution in C#

---
+++ URI_1009.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1009_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double totalSales = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double total = salary + (totalSales * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}

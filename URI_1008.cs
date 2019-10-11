9eae463a40d73abc55d5ec1568c5314e82185e67
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-10 21:00:44 -0300

    URI 1008 Solution in C#

---
+++ URI_1008.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1008_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double salaryPerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double salary = hours * salaryPerHour;
+
+            Console.WriteLine("NUMBER = " + number);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}

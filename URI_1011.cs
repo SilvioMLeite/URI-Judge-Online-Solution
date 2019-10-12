6e1f3c7dfddcdfb2de82ea0396835aa848006d68
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 03:47:15 -0300

    URI 1011 Solution in C#

---
+++ URI_1011.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1011_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14159;
+
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double volume = (4 / 3.0) * PI * Math.Pow(raio,3);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}

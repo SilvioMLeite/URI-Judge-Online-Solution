62435d675c41b036e2905db6b7783b8207c4a99c
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 07:19:09 -0300

    URI 1019 Solution in C#

---
+++ URI_1019.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1019_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int qtdSegHr = 3600;
+            const int qtdSegMn = 60;
+
+            int tempo = int.Parse(Console.ReadLine());
+
+            int hora = tempo / qtdSegHr;
+            int minutos = (tempo % qtdSegHr) / qtdSegMn;
+            int segundos = tempo % qtdSegMn;
+
+            Console.WriteLine(hora + ":" + minutos + ":" + segundos);
+        }
+    }
+}

9fe25aa34d5d2c5c9c470f161ac7740f63d94f82
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 05:06:07 -0300

    URI 1005 Solution in C#

---
+++ URI_1005.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1005_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double media = ((nota1 * 3.5) + (nota2 * 7.5)) / 11 ;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5",CultureInfo.InvariantCulture));
+        }
+    }
+}

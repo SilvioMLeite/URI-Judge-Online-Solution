ab352821031b21e4219b0f3ed9becdfff1f17083
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 07:31:55 -0300

    URI 1020 Solution in C#

---
+++ URI_1020.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_1020_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ano = 365;
+            const int mes = 30;
+
+            int idadeEmDias = int.Parse(Console.ReadLine());
+
+            int anos = idadeEmDias / ano;
+            int meses = (idadeEmDias % ano) / mes;
+            int dias = idadeEmDias % ano % mes;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+    }
+}

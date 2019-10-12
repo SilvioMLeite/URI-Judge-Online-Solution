53beebc955e4f4c9c819acdca2a54acdb3e4f0d5
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 06:05:44 -0300

    URI 1016 Solution in C#

---
+++ URI_1016.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI_1016_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora = 60;
+            
+            int distancia = int.Parse(Console.ReadLine());
+
+            int tempo  = hora * distancia / 30;
+
+            Console.WriteLine(tempo + " minutos");
+        }
+    }
+}

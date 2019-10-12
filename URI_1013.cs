fe95b6eb027e11c7fa4ec6b4535457f1e0a389dc
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 04:48:24 -0300

    URI 1013 Solution in C#

---
+++ URI_1013.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1013_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int valor1 = int.Parse(valores[0]);
+            int valor2 = int.Parse(valores[1]);
+            int valor3 = int.Parse(valores[2]);
+
+            int maiorAB = (valor1 + valor2 + Math.Abs(valor1 - valor2)) / 2;
+            int maior = (maiorAB + valor3 + Math.Abs(maiorAB - valor3)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}

628af43494df6e21628483109e21b9b1b642b8ea
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 06:29:37 -0300

    URI 1017 Solution in C#

---
+++ URI_1017.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1017_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double qtdLtHr = 12.0;
+
+            int tempo = int.Parse(Console.ReadLine());
+            int velocidadeMedia = int.Parse(Console.ReadLine());
+
+            double totallitros = tempo * velocidadeMedia / qtdLtHr;
+
+            Console.WriteLine(totallitros.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}

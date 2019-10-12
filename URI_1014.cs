a98c804b879f26e3c515c4726805d574421bdcd5
SilvioMLeite <silvio.marioleite@gmail.com>
2019-10-12 05:01:21 -0300

    URI 1014 Solution in C#

---
+++ URI_1014.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1014_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double consumo = x / y;
+
+            Console.WriteLine(consumo.ToString("F3",CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
